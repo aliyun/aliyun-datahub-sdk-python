c900aceeb6d921da1d7e38dd00abd0b4b2d3acf3
lewei <panjinxing.pjx@alibaba-inc.com>
2025-03-05 14:29:55 +0800

    [to #63929899] remove float128 for np

--- datahub/models/types.py
+++ datahub/models/types.py
@@ -261,7 +261,7 @@ try:
     import numpy as np
 
     integer_builtins += (np.integer, )
-    float_builtins += (np.float16, np.float32, np.float64, np.float128, )
+    float_builtins += (np.float16, np.float32, np.float64, )
     if np.__version__ < "1.20.0":
         float_builtins += (np.float, )
 except ImportError:
